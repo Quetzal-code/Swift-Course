be57c1c2be70f95b421b9615e84a49216b3dd37e
Quetzal <quetzaluzcategui@gmail.com>
2024-05-18 14:58:38 -0400

    Initial Commit

---
+++ Swift Course/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swift Course
+//
+//  Created by Quetzal on 18/5/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
