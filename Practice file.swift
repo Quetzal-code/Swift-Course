a2a31803a2b74e460fd1dc75ebcf349a1e225a8d
Quetzal <quetzaluzcategui@gmail.com>
2024-06-13 19:25:04 -0400

    Create Practice file.swift

---
+++ Practice file.swift
@@ -0,0 +1,83 @@
+//
+//  Practice file.swift
+//  Swift Course
+//
+//  Created by Quetzal on 13/6/24.
+//
+
+import Foundation
+
+// How to make a comment
+/*
+ Multiline comment
+ is created like this
+
+ */
+var greeting = "Hello, playground"
+// var is a reserved word
+// Use descriptive variables names
+
+// Variables declaration using ""
+
+// How to display something
+print(greeting)
+print("Display tex like this")
+
+//a variable that doesn't change is made using let
+let countryOfBirth = "Venezuela"
+
+// Changing the value of a variable
+var age = 5
+age = 6
+print(age)
+
+// Variables type
+// We could specify the data tyoe
+// var declaration using camelCase
+
+// Char
+
+// String
+var dogName = "Atlas"
+
+// Int
+var year:Int = 1990
+// Float
+var piFloat:Float = 3.142131
+
+// Double
+var piDouble:Double = 3.14124215151515151535464563463
+
+//Boolean
+var imHappy:Bool = true
+
+// Arithmetic Operators
+//The code is sequencial
+var a:Int = 5
+var b:Int = 6
+var sum = a + b
+var substract = a - b
+var multiply = a * b
+var divide = a / b
+
+//  Assignation Operators
+var example = 5
+example += 10
+example -= 6
+
+// Logical Operators
+
+let age2 = 14
+var couldDrive = age2 > 18
+print(couldDrive)
+
+let isNotEqual = age2 != 13
+print(isNotEqual)
+
+let isSunny = true
+let temperature = 25
+let isPleasant:Bool = temperature > 20 && isSunny
+// or ||
+let wearHat = !isSunny
+
+// Convertions
